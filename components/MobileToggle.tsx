8558acdf8d860a3823b7926616a7f31881718a00
unknown <dumbirichuks@gmail.com>
2024-01-31 13:26:22 +0100

    updated the mobile hook for toggling the side bar

--- components/MobileToggle.tsx
+++ components/MobileToggle.tsx
@@ -1,38 +1,61 @@
+"use client";
 
+import { useEffect, useState } from "react";
+import { usePathname } from "next/navigation";
 import { Menu } from "lucide-react"
+
+import { useMobileSidebar } from "@/hooks/useMobileSidebar";
 
 import {
     Sheet,
     SheetContent,
     SheetTrigger,
 } from "@/components/ui/sheet";
-
 import { Button } from "@/components/ui/button";
 import NavigationSidebar from "./navigation/NavigationSidebar";
 import { ScrollArea, ScrollBar } from "./ui/scroll-area";
 
 const MobileToggle = () => {
+
+    const pathname = usePathname();
+
+    const [isMounted, setIsMounted] = useState(false);
+
+    const onOpen = useMobileSidebar((state) => state.onOpen);
+    const onClose = useMobileSidebar((state) => state.onClose);
+    const isOpen = useMobileSidebar((state) => state.isOpen);
+
+    useEffect(() => {
+        setIsMounted(true)
+    }, []);
+
+    useEffect(() => {
+        onClose();
+    }, [pathname, onClose])
+
+    if (!isMounted) {
+        return null;
+    }
+
     return (
-        <div className="">
-            <Sheet>
-                <SheetTrigger>
-                    <Button
-                        variant="ghost"
-                        size="icon"
-                        className="md:hidden"
-                        Icon={<Menu />}
-                    />
-                </SheetTrigger>
-                <SheetContent side="left" className="border-none bg-white p-0 overflow-y-scroll no-scrollbar">
-                    <ScrollArea>
-                        <div className="">
-                            <NavigationSidebar />
-                        </div>
-                        <ScrollBar orientation="vertical" />
-                    </ScrollArea>
+        <>
+
+            <Button
+                onClick={onOpen}
+                variant="ghost"
+                size="icon"
+                className="md:hidden block"
+                Icon={<Menu />}
+            />
+
+            <Sheet open={isOpen} onOpenChange={onClose}>
+                <SheetContent
+                    side="left"
+                    className="border-none bg-white p-0 overflow-y-scroll no-scrollbar">
+                    <NavigationSidebar />
                 </SheetContent>
             </Sheet>
-        </div >
+        </ >
     )
 }
 
